63dcb8b483204dfbb684c57dc832f341c998d1d3
janhavi003 <janhavi.hivarekar@kalvium.community>
2026-04-15 16:12:20 +0530

    error debugged

--- server/src/index.ts
+++ server/src/index.ts
@@ -22,7 +22,7 @@ app.get("/", (req, res) => {
 });
 
 // Start server
-const PORT = 5000;
+const PORT = Number(process.env.PORT) || 5000;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
